e75ec17b7b1915a0f0562dc1f29b69a4ee9e1274
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-06-06 13:48:39 +0300

    create pin and add coordinate of pin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var createAd = function (xx, title, locationX, locationY, price, type, rooms, guests, checkin, features, description, photos, x, y) {
+var createAd = function (xx, title, price, type, rooms, guests, checkin, features, description, photos, x, y) {
   var ad =
   {
     author:
@@ -10,7 +10,7 @@ var createAd = function (xx, title, locationX, locationY, price, type, rooms, gu
     offer:
     {
       title: title,
-      address: locationX + ', ' + locationY,
+      address: location.x + ', ' + location.y,
       price: price,
       type: type,
       rooms: rooms,
@@ -29,41 +29,25 @@ var createAd = function (xx, title, locationX, locationY, price, type, rooms, gu
   return ad;
 };
 
-
-// var testAd =
-// {
-//   "author": {
-//       "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-//   },
-//   "offer": {
-//       "title": строка, заголовок предложения
-//       "address": строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-//       "price": число, стоимость
-//       "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-//       "rooms": число, количество комнат
-//       "guests": число, количество гостей, которое можно разместить
-//       "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-//       "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-//       "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-//       "description": строка с описанием,
-//       "photos": массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-//   },
-//   "location": {
-//       "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-//       "y": случайное число, координата y метки на карте от 130 до 630.
-//   }
-// };
-
 var createArrAds = function () {
   var arrAds = [];
-  var ad = createAd;
   for (var i = 0; i < 8; i++) {
+    var ad = createAd();
     arrAds.push(ad);
   }
+  return arrAds;
 };
 
 var map = document.querySelector('.map');
-map.classList.remove('.map--faded');
-console.log(map);
+map.classList.remove('map--faded');
+
+var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var ads = createArrAds();
+for (var i = 0; i < ads.length; i++) {
+  var left = ads[i].location.x - (pin.width / 2);
+  var topPin = ads[i].location.y - pin.height;
+  var style = 'style = left: ' + left + ' px; top: ' + topPin + ' px';
+  pin.classList.add(style);
+}
 
 createArrAds();
