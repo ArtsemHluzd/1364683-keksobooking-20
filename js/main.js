904fcaf3fe11f647c8e48c01c085a5067d01831f
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-06-06 14:45:42 +0300

    create function createPins

--- js/main.js
+++ js/main.js
@@ -38,16 +38,37 @@ var createArrAds = function () {
   return arrAds;
 };
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-var ads = createArrAds();
-for (var i = 0; i < ads.length; i++) {
-  var left = ads[i].location.x - (pin.width / 2);
-  var topPin = ads[i].location.y - pin.height;
-  var style = 'style = left: ' + left + ' px; top: ' + topPin + ' px';
-  pin.classList.add(style);
+var changeAttribute = function (element, attribute, value) {
+  element.removeAttribute(attribute);
+  element.setAttribute(attribute, value);
 }
 
-createArrAds();
+var createPins = function (ads) {
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var mapPins = document.querySelector('.map__pins');
+
+  for (var i = 0; i < ads.length; i++) {
+    var pin = pinTemplate.cloneNode(true);
+    var fragment = document.createDocumentFragment();
+
+    var left = ads[i].location.x - (pin.width / 2);
+    var topPin = ads[i].location.y - pin.height;
+    var style = 'style = left: ' + left + ' px; top: ' + topPin + ' px';
+    changeAttribute(pin, 'style', style);
+
+    var avatar = pin.querySelector('img');
+    var src = 'src= ' + ads[i].author.avatar;
+    var alt = 'alt= ' + ads[i].offer.title;
+    changeAttribute(avatar, 'src', src);
+    changeAttribute(avatar, 'alt', alt);
+
+    fragment.appendChild(pin);
+    mapPins.appendChild(fragment);
+  }
+};
+
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var ads = createArrAds();
+createPins(ads);
