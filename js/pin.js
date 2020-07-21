d1702b6dfd3a7db3a9728035d07693fcb25dadb3
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-07-21 12:23:45 +0300

    add fn insertAddressValue

--- js/pin.js
+++ js/pin.js
@@ -2,8 +2,10 @@
 
 (function () {
 
+  var MAIN_PIN_HEIGHT = 65;
   var MAP_PIN_WIDTH = 50;
   var MAP_PIN_HEIGHT = 70;
+  var MAP_LEFT_MERGIN = 250;
   var mainPin = document.querySelector('.map__pin--main');
   var addressInput = document.querySelector('#address');
   var ADS_LENGTH = 5;
@@ -55,31 +57,47 @@
       X: evt.clientX
     };
 
-    var onMousemoveMainPin = function (moveEvt) {
-      moveEvt.preventDefault();
 
-      var shift = {
-        y: startCoords.y - moveEvt.clientY,
-        x: startCoords.x - moveEvt.clientX
-      };
+    var insertAddressValue = function (shiftY = 0, shiftX = 0) {
 
-      startCoords = {
-        y: moveEvt.clientY,
-        x: moveEvt.clientX
-      };
-
-      var top = (mainPin.offsetTop - shift.y);
-      var left = (mainPin.offsetLeft - shift.x);
+      var top = (mainPin.offsetTop - shiftY);
+      var left = (mainPin.offsetLeft - shiftX);
 
       mainPin.style.left = left + 'px';
       mainPin.style.top = top + 'px';
       var addressValue = Math.round(left) + ', ' + Math.round(top);
       addressInput.value = addressValue;
+    };
+
+    var onMousemoveMainPin = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      if ((moveEvt.pageY - MAIN_PIN_HEIGHT / 2) > 0
+        && moveEvt.pageY < mainPin.parentNode.offsetHeight) {
+        // ограничение по Y реализовал, а по X не могу
+        // && moveEvt.pageX > 0
+        // && moveEvt.pageX < mainPin.parentNode.offsetWidth)
+
+        var shift = {
+          y: startCoords.y - moveEvt.clientY,
+          x: startCoords.x - moveEvt.clientX
+        };
+
+        startCoords = {
+          y: moveEvt.clientY,
+          x: moveEvt.clientX
+        };
+
+        insertAddressValue(shift.y, shift.x)
+
+      }
 
     };
 
     var onMouseupMainPin = function (upEvt) {
       upEvt.preventDefault();
+
+      insertAddressValue();
 
       window.removeEventListener('mousemove', onMousemoveMainPin);
       window.removeEventListener('mouseup', onMouseupMainPin);
