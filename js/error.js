672cb8bcc71242f7b0d0a7cff757980dee29d62b
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-07-28 16:29:44 +0300

    refactor: удалил лишние комментарии в коде

--- js/error.js
+++ js/error.js
@@ -4,11 +4,6 @@
   var main = document.querySelector('main');
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
 
-  // 2.7. Если при отправке данных произошла ошибка запроса, показывается
-  // соответствующее сообщение. Разметку сообщения, которая находится в блоке
-  // #error в шаблоне template, нужно разместить в <main>. Сообщение должно
-  // исчезать после нажатия на кнопку .error__button, по нажатию на клавишу Esc и
-  // по клику на произвольную область экрана за пределами блока с сообщением.
   var renderError = function () {
     var error = errorTemplate.cloneNode(true);
     var errorButtonElement = error.querySelector('.error__button');
