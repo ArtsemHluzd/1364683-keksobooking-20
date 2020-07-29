5ac495816a02e243ff2c4e19b1d9519417571418
Artsem Hluzd <artsem.hluzd@gmail.com>
2020-07-29 14:41:05 +0300

    refactor: вынес дублирование кода в отдельную функцию sendRequest

--- js/backend.js
+++ js/backend.js
@@ -4,7 +4,7 @@
   var LOAD_URL = 'https://javascript.pages.academy/keksobooking/data';
   var SAVE_URL = 'https://javascript.pages.academy/keksobooking';
 
-  var load = function (onSuccess, onError) {
+  var sendRequest = function (onSuccess, onError, type, url) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
@@ -14,21 +14,17 @@
         onError();
       }
     });
-    xhr.open('GET', LOAD_URL);
+    xhr.open(type, url);
+    return xhr;
+  };
+
+  var load = function (onSuccess, onError) {
+    var xhr = sendRequest(onSuccess, onError, 'GET', LOAD_URL);
     xhr.send();
   };
 
   var save = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr);
-      } else {
-        onError('Что-то пошло не так');
-      }
-    });
-    xhr.open('POST', SAVE_URL);
+    var xhr = sendRequest(onSuccess, onError, 'POST', SAVE_URL);
     xhr.send(data);
   };
 
